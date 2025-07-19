f630296c6d3b2b15abb81f2805308e21d424499f
DODO <dohae.kim22@gmail.com>
2025-07-19 21:19:15 +0200

    refactor: Enhance FormInput to support native input props with rest

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -1,29 +1,24 @@
+import { InputHTMLAttributes } from "react";
+
 interface FormInputProps {
-  type: string;
   name: string;
-  placeholder: string;
-  required: boolean;
   errors?: string[];
   defaultValue?: string;
 }
 
 export default function FormInput({
-  type,
   name,
-  placeholder,
-  required,
-  errors,
+  errors = [],
   defaultValue,
-}: FormInputProps) {
+  ...rest
+}: FormInputProps & InputHTMLAttributes<HTMLInputElement>) {
   return (
     <div className="flex flex-col gap-2">
       <input
         className="bg-transparent border-none rounded-md focus:outline-none transition h-10 ring-1 ring-neutral-200 focus:ring-2 focus:ring-orange-500 placeholder:text-neutral-400"
-        type={type}
         name={name}
-        placeholder={placeholder}
-        required={required}
         defaultValue={defaultValue}
+        {...rest}
       />
       {errors &&
         errors.map((error) => (
