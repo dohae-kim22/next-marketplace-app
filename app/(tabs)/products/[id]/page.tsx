f1b7b4e37137d47aac85989db779764065395e40
DODO <dohae.kim22@gmail.com>
2025-07-21 14:31:50 +0200

    feat: Add skeleton UI for product detail page

---
+++ app/(tabs)/products/[id]/page.tsx
@@ -0,0 +1,13 @@
+async function getProduct() {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+}
+
+export default async function ProductDetail({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const product = await getProduct();
+  return <span>Product detail of the product {id}</span>;
+}
