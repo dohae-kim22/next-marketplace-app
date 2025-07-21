936ccba3bd6a3097b1e69b71e427cca77af35a77
DODO <dohae.kim22@gmail.com>
2025-07-21 15:10:56 +0200

    feat: Create product detail page

--- app/products/[id]/loading.tsx
+++ app/products/[id]/loading.tsx
@@ -2,12 +2,12 @@ import { PhotoIcon } from "@heroicons/react/24/solid";
 
 export default function Loading() {
   return (
-    <div className="flex flex-col gap-2 animate-pulse p-5">
+    <div className="flex flex-col gap-3 animate-pulse p-5">
       <div className="flex justify-center items-center aspect-square border-neutral-700 text-neutral-700 border-4 border-dashed rounded-md">
         <PhotoIcon className="h-22" />
       </div>
       <div className="flex gap-2 items-center">
-        <div className="size-14 rounded-full bg-neutral-700" />
+        <div className="size-12 rounded-full bg-neutral-700" />
         <div className="flex flex-col gap-1">
           <div className="h-4 w-40 bg-neutral-700 rounded-md" />
           <div className="h-4 w-20 bg-neutral-700 rounded-md" />
---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,100 @@
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { UserIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+import { formatToEuro } from "@/lib/utils";
+
+async function getProduct(id: number) {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      user: {
+        select: {
+          userName: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+
+  return product;
+}
+
+async function getIsOwner(userId: number) {
+  const session = await getSession();
+  if (session.id) {
+    return session.id === userId;
+  }
+  return false;
+}
+
+export default async function ProductDetail({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const numericId = Number(id);
+
+  if (isNaN(numericId)) {
+    return notFound();
+  }
+
+  const product = await getProduct(numericId);
+
+  if (!product) {
+    return notFound();
+  }
+
+  const isOwner = await getIsOwner(product.userId);
+
+  return (
+    <div className="flex flex-col gap-3 p-5">
+      <div className="relative aspect-square rounded-md overflow-hidden">
+        <Image fill src={product.photo} alt={product.title} />
+      </div>
+      <div className="flex gap-2 items-center border-neutral-700 border-b pb-3">
+        <div className="flex justify-center items-center size-12 rounded-full bg-neutral-700">
+          {product.user.avatar !== null ? (
+            <Image
+              src={product.user.avatar}
+              width={40}
+              height={40}
+              alt={product.user.userName}
+            />
+          ) : (
+            <UserIcon className="size-8" />
+          )}
+        </div>
+        <div>
+          <h3>{product.user.userName}</h3>
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <h1 className="text-xl font-semibold">{product.title}</h1>
+        <p className="text-sm">{product.description}</p>
+      </div>
+      <div className="fixed w-full bottom-0 p-5 left-0 bg-neutral-800 flex justify-between items-center">
+        <span className="font-semibold text-xl">
+          €{formatToEuro(product.price)}
+        </span>
+        {isOwner ? (
+          <button className="bg-red-500 px-5 py-2.5 rounded-md text-white font-semibold hover:bg-red-400">
+            Delete product
+          </button>
+        ) : (
+          <Link
+            className="bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold hover:bg-orange-400"
+            href={``}
+          >
+            Ask seller
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+}
